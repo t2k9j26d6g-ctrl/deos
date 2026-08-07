8e1cab1f5d7d80d5d98fae11c0095806cdde1b0d
Ludovic Aoust <ludovic_aoust@dg.carrefour.com>
2026-08-07 17:36:37 +0200

    V5.21G.1 - publie la configuration Supabase multi-appareils

---
+++ config.js
@@ -0,0 +1,14 @@
+window.DEOS_REMOTE_CONFIG = {
+  enabled: true,
+  provider: "supabase",
+
+  supabaseUrl: "https://pgcmvzmdyydoswaxacvk.supabase.co",
+  supabasePublishableKey: "sb_publishable_nJqaiGzOB1tLYh1pE30Vnw_GLOJ8z73",
+
+  environment: "test",
+
+  authRedirectUrl:
+    window.location.origin + window.location.pathname,
+
+  debug: false
+};
